53c852ffbf6d9e955d682ca5f273d4396fbe9157
Sam Weaver <stweaver@ncsu.edu>
2018-04-29 23:48:16 -0400

    Good point where we have automatically updating stories and everything thus far.

--- app.js
+++ app.js
@@ -43,39 +43,58 @@ window.extractRootDomain = function(url) {
 
 Vue.prototype.extractRootDomain = window.extractRootDomain;
 
-window.storyLoadFunction = function() {
-  // in this we fetch the data based off the ID passed to the item
+window.storyUpdateFunction = function(fbItem) {
+  console.log("Story #" + this.item.id + " updated.");
+  let data = fbItem.val();
+  // console.log(data);
+  this.item.title = data.title;
+  this.item.pointCount = data.score;
+  this.item.commentText =
+    data.descendants == 0 ? "discuss" : data.descendants + " comments";
+  this.item.username = data.by;
+  this.item.time = new Date(1000 * data.time);
+  this.item.story = data.type == "story";
+  this.item.href = data.url;
+  if (!data.url) {
+    this.item.href = "https://news.ycombinator.com/item?id=" + this.item.id;
+    this.item.text = true;
+  }
+};
+
+window.newsLoadFunction = function() {
+  // in this we attach a listener for the
+  // data based off the ID passed to the item
+
+  console.log("Story #" + this.item.id + " is now listening for updates.");
 
   window.db
     .child("item")
     .child(this.item.id)
-    .once("value")
-    .then(
-      function(fbItem) {
-        let data = fbItem.val();
-        this.item.title = data.title;
-        this.item.pointCount = data.score;
-        this.item.commentText =
-          data.descendants == 0 ? "discuss" : data.descendants + " comments";
-        this.item.username = data.by;
-        this.item.time = new Date(1000 * data.time);
-        this.item.story = data.type == "story";
-        this.item.href = data.url;
-        if (!data.url) {
-          this.item.href =
-            "https://news.ycombinator.com/item?id=" + this.item.id;
-          this.item.text = true;
-        }
-      }.bind(this)
-    );
+    .on("value", storyUpdateFunction.bind(this));
+};
+
+window.newsUnloadFunction = function() {
+  // in this we detach the listener attached on load
+
+  // if for some reason we never had an item, we can ignore it
+  if (!this.item) return;
+
+  console.log(
+    "Story #" + this.item.id + " is no longer listening for updates."
+  );
+
+  window.db
+    .child("item")
+    .child(this.item.id)
+    .off("value");
 };
 
 // define the component for a news item
 // by the way this is hideous and gross and I know.
 Vue.component("news-item", {
   props: ["item", "index"],
-  created: storyLoadFunction,
-  updated: storyLoadFunction,
+  created: newsLoadFunction,
+  beforeDestroy: newsUnloadFunction,
   template:
     "<div>" +
     "<!-- Item title line -->" +
